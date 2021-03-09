a669cc91e7590bc6af2d145bdf2f9178d30bebc6
mariasharmagbay <mariasharmagbay@gmail.com>
2021-03-08 22:00:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-console.log("Hello world!!");
+// console.log("Hello world!!");
 
-person = 'Sharmin';
-character = {
-    name: person,
-    age: 21
-}
+// person = 'Sharmin';
+// character = {
+//     name: person,
+//     age: 21
+// }
 
-console.log(person);
-console.log(character);
+// console.log(person);
+// console.log(character);
+//-----------------------------
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+    console.log('Request Received')
+    response.json({msg: 'Welcome'});
+})
+
+app.listen(8888, () => {
+    console.log('THe server is up and listening on port 3333')
+})
+
